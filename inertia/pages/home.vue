44ec5f82a7075522103de270898ad5543f0dd652
RobinWiilog <robin.pluviaux@wiilog.fr>
2024-08-12 17:08:13 +0200

    first commit

---
+++ inertia/pages/home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+
+</script>
+
+<template>
+  <Head title="Homepage" />
+  <span class="text-red-500 font-bold">URL Insight</span>
+</template>
