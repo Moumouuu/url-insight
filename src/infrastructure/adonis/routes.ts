ef0eeb429983ebe2066a56e81d50c064699fb2a1
RobinWiilog <robin.pluviaux@wiilog.fr>
2024-08-13 11:25:50 +0200

    feat(): create routes API

--- src/infrastructure/adonis/routes.ts
+++ src/infrastructure/adonis/routes.ts
@@ -8,5 +8,21 @@
 */
 
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home', { version: 6 })
+import SessionController from "#controllers/session_controller";
+import { middleware } from "#infrastructure/adonis/kernel";
+import TokensController from "#controllers/tokens_controller";
+import UrlsController from "#controllers/urls_controller";
 
+router.on('/').renderInertia('home')
+router.get('/login', [SessionController, 'retrieve']).as('login')
+router.post('/register', [SessionController, 'store']).as('register')
+router.delete('/logout', [SessionController, 'destroy']).as('logout').use(middleware.auth())
+
+
+router.group(() => {
+  // Routes for authenticated users
+  router.get('/me', [TokensController, 'retrieve']).as('me')
+  router.get('/urls', [UrlsController, 'retrieve']).as('urls')
+  router.post('/urls', [UrlsController, 'store']).as('urls.store')
+  router.delete('/urls/:id', [UrlsController, 'destroy']).as('urls.destroy')
+}).use(middleware.auth())
